ee6113ee0572e24cbc08f676e59aca4c75efb32c
Vaibhav Shringarpure <vibs.sh@gmail.com>
2018-07-28 14:18:54 +0100

    * Header Styling and Layout

--- Client/chore-app/src/app/components/header/header.component.ts
+++ Client/chore-app/src/app/components/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'cho-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+    appTitle: string = "Chore";
+    subTitle: string = "Keeping you on your toes";
+    today: any;
+    month: any =  ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   constructor() { }
 
   ngOnInit() {
+      this.todayDate();
   }
 
+  todayDate() {
+    let d = new Date();
+    let currentMonth = this.month[d.getMonth()];
+    this.today = currentMonth + " " + d.getDate() + ", " + d.getFullYear();
+  }
 }
